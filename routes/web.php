907a448d9b61aae48121c71da595be1798403333
MTegarRosmali <02tegarrosmali@gmail.com>
2022-10-17 20:05:28 +0700

    Pertemuan 6 Menyelesaikan video 3

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,19 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "M.Tegar Nurul Fuad Rosmali",
+        "email" => "02tegarrosmali@gmail.com",
+        "image" => "profile.jpeg"
+    ]);
+});
+
+
+Route::get('/blog', function () {
+    return view(posts);
+});
+
