4ecf843aab376fb533c21392a68c4f0b2483266f
MTegarRosmali <02tegarrosmali@gmail.com>
2022-10-26 20:21:33 +0700

    Video 8 yang belum selesai

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,6 +4,8 @@
 
     <h2 class="mb-5">{{ $post->title }}</h2>
 
+    <p>By. M.Tegar Nurul Fuad Rosmali in {{ $post->category->name }}</p>
+
     {!! $post->body !!}
 
     <a href="/blog">Kembali</a>
