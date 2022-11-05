a7868ce93c8fbca4feada926021f0b3e248ceaed
MTegarRosmali <02tegarrosmali@gmail.com>
2022-11-05 22:04:43 +0700

    pertemuan 8 menyelesaikan video 13 Laravel

--- routes/web.php
+++ routes/web.php
@@ -39,6 +39,7 @@ Route::get('/about', function () {
 
 
 Route::get('/blog', [PostController::class, 'index']);
+Route::get('/posts', [PostController::class, 'index']);
 Route::get('/posts/{post:slug}', [PostController::class, 'show']);
 
 Route::get('/categories', function() {
@@ -46,20 +47,5 @@ Route::get('/categories', function() {
         'title' => 'Post Categories',
         'active' => 'categories',
         'categories' => Category::all()
-    ]);
-});
-
-Route::get('/categories/{category:slug}', function(Category $category) {
-    return view ('posts', [
-        'title' => "Post By Category : $category->name",
-        'active' => 'categories',
-        'posts' => $category->posts->load('category', 'author')
-    ]);
-});
-
-Route::get('/authors/{author:username}', function(User $author) {
-    return view ('posts', [
-        'title' => "Post By Author : $author->name",
-        'posts' => $author->posts->load('category', 'author'),
     ]);
 });
